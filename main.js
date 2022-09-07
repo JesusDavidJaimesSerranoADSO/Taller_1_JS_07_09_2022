5c382abf90c8043948992a506cc95bb3e56bb3c5
JesúsDavidJaimesSerranoADSO <jdavidjaimess@gmail.com>
2022-09-07 10:20:07 -0500

    Ejercicio 7

--- main.js
+++ main.js
@@ -1,18 +1,11 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 5. Escribir un programa que calcule la longitud y el área de una circunferencia: Radio = 4
-    // Longitud de la circunferencia = 2 * PI * radio
-    // Área de la circunferencia = PI * radio˄2
+    // Escribir un programa que calcule el volumen de una esfera:
+    // Radio = 3 volumen de la esfera = 4/3 * PI * radio˄3
+    let radio = Number(prompt("Ingrese el radio de la esfera", 3));
 
-
-    let radio = Number(prompt("Ingrese el radio de la circunferencia", 5));
-
-
-    console.group(`%cFormula ${2} * ${Math.PI} * ${radio}`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
-
-        console.log(`Longitud de la circunferencia : "${2 * Math.PI * radio}"`);
+    console.group();
+        console.log(`calcule el volumen de una esfera : "${(4/3) * Math.PI * Math.pow(radio,3)}"`);
     console.groupEnd();
 
-    console.group(`%cFormula ${Math.PI} * ${Math.pow(radio, 2)}`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
-        console.log(`Área de la circunferencia : "${Math.PI * Math.pow(radio, 2)}"`);
-    console.groupEnd();
+
 })
