f186e95f8e52e5a601e526a87a5bf6f1cc7dca5a
JesúsDavidJaimesSerranoADSO <jdavidjaimess@gmail.com>
2022-09-07 10:38:14 -0500

    Ejercicio 8

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // Escribir un programa que calcule el volumen de una esfera:
-    // Radio = 3 volumen de la esfera = 4/3 * PI * radio˄3
-    let radio = Number(prompt("Ingrese el radio de la esfera", 3));
+    // Escribir un programa que evalúe la siguiente expresión:
+    // (a+7*c)/(b+2-a)+2*b
+    let valor1 = Number(prompt("Ingrese el primer valor", 3));
+    let valor2 = Number(prompt("Ingrese el segundo valor", 5));
+    let valor3 = Number(prompt("Ingrese el tercer  valor", 8));
 
     console.group();
-        console.log(`calcule el volumen de una esfera : "${(4/3) * Math.PI * Math.pow(radio,3)}"`);
+        console.log(`la siguiente expresión: : "(${(valor1+7*valor3)} / ${valor2+2-valor1} + ${2*valor2})"`);
+    console.groupEnd();
+    console.group();
+        console.log(`calcule el volumen de una esfera : "${(valor1+7*valor3)/(valor2+2-valor1)+2*valor2}"`);
     console.groupEnd();
 
 
