c51f2e9661e1dfa424be7e7d28e4d426e78e2e72
JesúsDavidJaimesSerranoADSO <jdavidjaimess@gmail.com>
2022-09-14 08:50:00 -0500

    Ejercicio 15

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 addEventListener("DOMContentLoaded", ()=>{
-//     Una farmacia aplica al precio de los remedios el 10% de descuento, hacer un programa que
-// ingresando el costo de los medicamentos calcules el descuento y el precio final.
-    let valor = Number(prompt(`ingrese el valor del medicamento`,3600));
-    let desc = valor - (valor * 0.10)
-
-    console.log(`el valor del medicamento es de ${desc}`)
+//     Hacer un diagrama para convertir de grados centígrados a grados Fahrenheit.
+    let valor = Number(prompt(`ingrese la temperatura`,32));
+    let convercion = valor * 1.8 + 32
+    
+    console.log(`la temperatura en grados Fahrenheit es ${convercion}`)
     }) 
