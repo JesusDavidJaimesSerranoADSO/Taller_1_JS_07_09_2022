2e25a50d5dd43d3b6e455ef515d9618d3374fb3c
JesúsDavidJaimesSerranoADSO <jdavidjaimess@gmail.com>
2022-09-14 10:12:25 -0500

    Ejercicio 20

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // Un estudiante realiza cuatro exámenes. Realizar el pseudocódigo que representen el algoritmo
-    // correspondiente para obtener el promedio de las calificaciones obtenidas. las calificaciones van 1 a 5
-    // puntos.
-    let valor1 = Number(prompt(`ingrese la nota del examen 1`,5));
-    let valor2 = Number(prompt(`ingrese la nota del examen 2`,6));
-    let valor3 = Number(prompt(`ingrese la nota del examen 3`,5));
-    let valor4 = Number(prompt(`ingrese la nota del examen 4`,6));
-    let prom = (valor1 + valor2 + valor3 + valor4) / 4
+//     Un vendedor recibe un sueldo base más el 10% de comisión sobre sus ventas. Si en un mes
+// cualquiera hace tres ventas por valores: v1, v2 y v3, ¿cuánto recibirá por comisión? y ¿cuánto en total
+// sueldo del vendedor?.
+    let valor1 = Number(prompt(`ingrese el valor de la venta 1`,5000));
+    let valor2 = Number(prompt(`ingrese el valor de la venta 2`,6000));
+    let valor3 = Number(prompt(`ingrese el valor de la venta 3`,35000));
+    let sbase = 1000000
+    let com = (valor1 + valor2 + valor3) * 0.10
 
-    console.log(`el promedio de las notas es de ${prom}`)
+    console.log(`el valor de la comision es de ${com}`)
+    console.log(`el sueldo total del trabajador es de ${sbase + com}`)
     }) 
